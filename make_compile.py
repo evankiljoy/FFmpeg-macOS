166cc8b7faff407b83af3103c8123184ada7c54a
Evan Short <evan.kiljoy@gmail.com>
2024-06-01 14:19:02 -0400

    Update make_compile.py

--- make_compile.py
+++ make_compile.py
@@ -9,6 +9,13 @@ def execute(command: str):
     print(f"Execute: {command}.")
     os.system(command)
 
+def get_macos_sdk_path():
+    try:
+        sdk_path = subprocess.check_output(['xcrun', '--show-sdk-path']).strip()
+        return sdk_path.decode('utf-8')
+    except subprocess.CalledProcessError as e:
+        sys.stderr.write(f"An error occurred: {e}\n")
+        sys.exit(1)
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Configure & Make & Install FFmpeg.")
@@ -28,9 +35,14 @@ if __name__ == "__main__":
     def make(arch: str):
         n_cpu = cpu_count()
         print("Configure project.")
+        osx_sdk = get_macos_sdk_path()
+        osx_version = "10.12" if arch == "x86_64" else "11.0"
         execute(
             f"cd {ffmpeg_dir} && ./configure --enable-cross-compile --prefix={target_dir / ('install_' + arch + '/')} "
-            f"--enable-shared --disable-static --arch={arch} --cc='clang -arch {arch}'"
+            f"--enable-shared --disable-static --arch={arch} --cc='clang -arch {arch}' "
+            f"--disable-programs --disable-avdevice --enable-avresample --enable-opencl --enable-lto "
+            f"--extra-ldflags='-isysroot {osx_sdk} -mmacosx-version-min={osx_version} -flto -fuse-linker-plugin' "
+            f"--extra-cflags='-isysroot {osx_sdk} -mmacosx-version-min={osx_version}' "
         )
         print(f"Make project ({n_cpu} threads).")
         execute(f"cd {ffmpeg_dir} && make -j{n_cpu}")
