84c7240409549450cdd6f14af52942175b2deab8
Evan Short <evan.kiljoy@gmail.com>
2024-06-01 15:43:24 -0400

    Update package.py

--- package.py
+++ package.py
@@ -9,16 +9,17 @@ if __name__ == "__main__":
     parser.add_argument("--dir", type=str, default=os.getcwd(), help='indicate target dir.')
     parser.add_argument("--tag", type=str, default="UNKNOWN", help='indicate FFmpeg tag.')
     args = parser.parse_args()
-    target_dir = pathlib.Path(args.dir).absolute()
-    tag = args.tag
-    print(f"Packaging... {target_dir} {tag}")
-    install_intel_dir = target_dir / "install_x86_64"
-    install_apple_dir = target_dir / "install_arm64"
-    install_universal_dir = target_dir / "install_universal"
-
-    shutil.make_archive(f"FFmpeg-shared-{tag}-OSX-arm64", "zip", install_apple_dir)
-    print("Finished arm64.")
-    shutil.make_archive(f"FFmpeg_shared-{tag}-OSX-x86_64", "zip", install_intel_dir)
-    print("Finished x86_64.")
-    shutil.make_archive(f"FFmpeg-shared-{tag}-OSX-universal", "zip", install_universal_dir)
-    print("Finished universal.")
+    for config in ["Release", "Debug"]:
+        target_dir = pathlib.Path(args.dir).absolute() / config
+        tag = args.tag
+        print(f"Packaging... {target_dir} {tag}")
+        install_intel_dir = target_dir / "install_x86_64"
+        install_apple_dir = target_dir / "install_arm64"
+        install_universal_dir = target_dir / "install_universal"
+    
+        shutil.make_archive(f"FFmpeg-shared-{tag}-{config}-OSX-arm64", "zip", install_apple_dir)
+        print("Finished arm64.")
+        shutil.make_archive(f"FFmpeg_shared-{tag}-{config}-OSX-x86_64", "zip", install_intel_dir)
+        print("Finished x86_64.")
+        shutil.make_archive(f"FFmpeg-shared-{tag}-{config}-OSX-universal", "zip", install_universal_dir)
+        print("Finished universal.")
